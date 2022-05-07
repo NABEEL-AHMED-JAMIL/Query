9bceb14122ee8bd46d0e4e56e1b573262acb51cf
Nabeel Ahmed <nabeel.amd93@gmail.com>
2022-05-07 13:07:24 +0300

    Age Group Query for 2D

---
+++ age group.sql
@@ -0,0 +1,26 @@
+select * from customers;
+
+-- 'Under 18' -> 0
+-- '18-24' -> 1
+-- '25-34' -> 2
+-- 'Other' -> 3
+
+SELECT
+    CASE
+        WHEN cust.age <18 THEN '0'
+        WHEN cust.age BETWEEN 18 AND 24 THEN '1'
+        WHEN cust.age BETWEEN 25 AND 34 THEN '2'
+        ELSE '3' 
+    END AS age_range, cust.state, count(*) AS TOTAL
+FROM (SELECT DISTINCT customer_id, state, EXTRACT(YEAR from AGE(date_of_birth))::INT AS age FROM customers) as cust
+GROUP BY age_range, cust.state
+ORDER BY age_range ASC;
+
+SELECT DISTINCT customer_id, EXTRACT(YEAR from AGE(date_of_birth))::INT AS age
+FROM customers
+WHERE EXTRACT(YEAR from AGE(date_of_birth))::INT < 18;
+
+SELECT DISTINCT customer_id, EXTRACT(YEAR from AGE(date_of_birth))::INT AS age
+FROM customers
+WHERE EXTRACT(YEAR from AGE(date_of_birth))::INT > 34;
+
---
+++ dummy date of birth.sql
@@ -0,0 +1,9 @@
+// create a tale 
+
+select * from customers;
+
+ALTER TABLE customers
+ADD COLUMN date_of_birth DATE;
+
+UPDATE customers SET date_of_birth = NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 100);
+
